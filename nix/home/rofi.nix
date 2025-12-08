eccedcfc6521657a022a74ed175dfcd81cb33448
Client <client@clientcrash.com>
2025-12-09 00:38:17 +0100

    fix rofi theme

--- nix/home/rofi.nix
+++ nix/home/rofi.nix
@@ -1,12 +1,30 @@
 { config, pkgs, ... }:
 
+let
+  catppuccinRofiVersion = "71fb15577ccb091df2f4fc1f65710edbc61b5a53";
+  
+
+  catppuccinDefaultRasi = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/rofi/${catppuccinRofiVersion}/catppuccin-default.rasi";
+    sha256 = "1f3r6yarrykj8cxvi5hblzlr5n8zbncifnxps9xl5gl32w6ysq5z";
+  };
+  
+  catppuccinDefaultMocha = pkgs.writeText "catppuccin-default-mocha.rasi" ''
+    @import "catppuccin-mocha.rasi"
+    
+    ${builtins.readFile catppuccinDefaultRasi}
+  '';
+
+in
 {
   xdg.configFile."rofi/themes/catppuccin-mocha.rasi".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/rofi/71fb15577ccb091df2f4fc1f65710edbc61b5a53/themes/catppuccin-mocha.rasi";
+    url = "https://raw.githubusercontent.com/catppuccin/rofi/${catppuccinRofiVersion}/themes/catppuccin-mocha.rasi";
     sha256 = "sha256-iyh6RcbIw5tX1PdzMmOLYvYjA2YVqdfJ/J6lJZgHdkY=";
   };
 
+  xdg.configFile."rofi/themes/catppuccin-default.rasi".source = catppuccinDefaultMocha;
+
   xdg.configFile."rofi/config.rasi".text = ''
-    @theme "catppuccin-mocha"
+    @theme "catppuccin-default"
   '';
 }
